2f1df4c1b1eff4ada59aef782d8a780e707fb3aa
FX Soubirou <soubirou@yahoo.fr>
2016-11-20 11:39:05 +0100

    Add ng2-bootstrap - fix: #2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewContainerRef } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  private viewContainerRef: ViewContainerRef;
   footer = 'tam4.io v x.y.z - bbb &copy; 2016';
+
+  public constructor(viewContainerRef: ViewContainerRef) {
+    // You need this small hack in order to catch application root view container ref
+    this.viewContainerRef = viewContainerRef;
+  }
 }
