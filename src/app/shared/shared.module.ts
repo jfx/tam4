55aebd9f2ec102a483e546c2971bc9d40ab00cad
FX Soubirou <soubirou@yahoo.fr>
2017-01-01 21:59:11 +0100

    Fix license header - Fix: #23

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -8,13 +8,13 @@
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * eco4 is distributed in the hope that it will be useful,
+ * tam4 is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with eco4. If not, see <http://www.gnu.org/licenses/>.
+ * along with tam4. If not, see <http://www.gnu.org/licenses/>.
  */
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
