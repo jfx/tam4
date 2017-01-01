36a2d65e7b39e67be2e46936be6abf8fcb3a1d3a
FX Soubirou <soubirou@yahoo.fr>
2017-01-01 21:49:07 +0100

    License header - fix: #23

--- src/app/core/service/test/fake-action.service.ts
+++ src/app/core/service/test/fake-action.service.ts
@@ -5,7 +5,7 @@ import { Observable } from 'rxjs/Observable';
 import { Action } from 'app/personal/shared/action.model';
 import { ActionService } from '../action/action.service';
 
-export var TESTACTIONS: Action[] = [
+export let TESTACTIONS: Action[] = [
     new Action('41', 'Title 1', 1, 2, 2, 'Desc 1'),
     new Action('42', 'Title 2', 3, 4, 1, 'Desc 2', '2016-12-31'),
 ];
--- src/app/personal/today/today.component.ts
+++ src/app/personal/today/today.component.ts
@@ -1,3 +1,21 @@
+/**
+ * Copyright (c) 2017 Francois-Xavier Soubirou.
+ *
+ * This file is part of tam4.
+ *
+ * tam4 is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * eco4 is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with eco4. If not, see <http://www.gnu.org/licenses/>.
+ */
 import { Component, OnInit } from '@angular/core';
 
 import 'rxjs/add/operator/toPromise';
