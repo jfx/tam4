27525f454caf1edc09735156ddc5e431057d0891
FX Soubirou <soubirou@yahoo.fr>
2017-02-18 22:13:58 +0100

    Replace ng2-dnd by ng2-dragula Drag and Drop package

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -19,20 +19,17 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { DndModule } from 'ng2-dnd';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
-    DndModule.forRoot(),
   ],
   declarations: [
   ],
   exports: [
     CommonModule,
     FormsModule,
-    DndModule,
   ],
   providers: [],
   bootstrap: []
